8cebf28e71ffcd3f57371a4dc46cb5cb3e5d8e65
Chris Schanot <47960200+cschanot@users.noreply.github.com>
2021-02-23 18:41:24 -0500

    Add files via upload

--- detect2.py
+++ detect2.py
@@ -2,9 +2,10 @@ from imageai.Detection import ObjectDetection
 import os
 import sys
 import glob
-import telegram
+# import telegram (use either telegram or Pushover )
 from datetime import datetime
 import configparser
+from pushover import Client
 
 config_file = "alerts.ini"
 
@@ -13,8 +14,12 @@ config.read(config_file)
 now = datetime.now()
 date_time = now.strftime("%m%d%Y%H%M%S")
 
-my_chatID = '1499804078'
-token = "1506875212:AAE5YkRKA3s2og5TTu3-haTK4z9YlaMelCw"
+# Telegram
+#my_chatID = '1499804078'
+#token = "1506875212:AAE5YkRKA3s2og5TTu3-haTK4z9YlaMelCw"
+# pushover
+my_push_token = 'ay9kn4biebdaz8mzgw3ic3dmgn2qd9'
+my_pushID = 'uzmbs8vh2vjib49284uids6wejbn6m'
 
 alerts_path = "E:\\BlueIris\\Alerts\\"
 execution_path = "C:\\BlueIris\\ai\\"
@@ -41,16 +46,21 @@ for eachObject in detections:
     # print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
     if(eachObject["name"] == "person"):
        print("We detected a person with a "+str(round(eachObject["percentage_probability"],2))+"% probability")
-       bot = telegram.Bot(token=token)
-       photo=open(os.path.join(output_path , date_time+"_"+os.path.basename(LatestFile)), 'rb')
-       bot.send_message(chat_id=my_chatID, text="We detected a person with a "+str(round(eachObject["percentage_probability"],2))+"% probability")
-       bot.sendPhoto(chat_id=my_chatID, photo=photo)
-       photo.close()
+       # Pushover
+       client = Client(my_pushID, api_token=my_push_token)
+       # Telegram
+       #bot = telegram.Bot(token=token)
+       with open(os.path.join(output_path , date_time+"_"+os.path.basename(LatestFile)), 'rb') as image:
+           client.send_message("We detected a person with a "+str(round(eachObject["percentage_probability"],2))+"% probability", attachment=image)
+           #bot.send_message(chat_id=my_chatID, text="We detected a person with a "+str(round(eachObject["percentage_probability"],2))+"% probability")
+           #bot.sendPhoto(chat_id=my_chatID, photo=image)
+           image.close()
        break # We dont want multiple messages with the same image
     else:
        print("We did not detect a person")
     print("--------------------------------")
 
 ## Clean Up
+#photo.close()
 #if os.path.exists(os.path.join(output_path , date_time+"_"+os.path.basename(LatestFile))):
 #    os.remove(os.path.join(output_path , date_time+"_"+os.path.basename(LatestFile)))
